192a4495ff02fb026ad063c44db80b9dbca5ddf9
Mandar Gogate <9411881+MandarGogate@users.noreply.github.com>
2021-06-02 09:43:33 +0100

    updated contact

--- js/validation.js
+++ js/validation.js
@@ -8,10 +8,9 @@ $(document).ready(function(){
             var error = false;
             var name = $('#name').val();
             var email = $('#email').val();
-			var phone = $('#phone').val();
             var message = $('#message').val();
 			
-			$('#name,#email,#phone,#message').click(function(){
+			$('#name,#email,#message').click(function(){
 				$(this).removeClass("error_input");
 			});
             
@@ -27,12 +26,6 @@ $(document).ready(function(){
                 $('#email').addClass("error_input");
             }else{
                 $('#email').removeClass("error_input");
-            }
-			if(phone.length == 0){
-                var error = true;
-                $('#phone').addClass("error_input");
-            }else{
-                $('#phone').removeClass("error_input");
             }
             if(message.length == 0){
                 var error = true;
